50acaa608d81c3d69c763df65ec42d831fa58b6d
E. R. Wang <01ewang24@gmail.com>
2023-03-20 23:34:47 -0600

    finished example

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+const path = require('path')
+
 const { app, BrowserWindow } = require('electron')
-    const createWindow = () => {
-         const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+       preload: path.join(__dirname, 'src', 'preload.js') 
+    }
+  });
 
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 }
-
 app.whenReady().then(() => {
   createWindow()
 })
