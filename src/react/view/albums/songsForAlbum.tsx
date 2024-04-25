681e4dbe480cec56e1f7ae73ea7800e8d75dde84
E. R. Wang <01ewang24@gmail.com>
2024-04-24 22:15:40 -0600

    set up some sass variables and formatting the songs table

--- src/react/view/albums/songsForAlbum.tsx
+++ src/react/view/albums/songsForAlbum.tsx
@@ -5,7 +5,7 @@ import ViewContainer from '../global/viewContainer';
 
 const SongsForAlbum: React.FC<{ album: Album, closeSongsForAlbumView: Function }> = ({ album, closeSongsForAlbumView }) => {
 
-    const [songs, setSongs] = useState<Song[]>(undefined)
+    const [songs, setSongs] = useState<Song[]>(undefined);
     useEffect(() => {
         (window as any).electron.ipcRenderer.invoke('getSongsByAlbum', album.id)
             .then((songs: Song[]) => {
@@ -14,19 +14,62 @@ const SongsForAlbum: React.FC<{ album: Album, closeSongsForAlbumView: Function }
     }, [])
 
     return <ViewContainer>
-        <div className='p-row'>
+        <div className='p-row p-row-space-between'>
+            <div className='p-row p-row-flex-start'>
+                <div className='album-songs-cover'>
+                    <img
+                        src='../../assets/img/test.jpg'
+                    />
+                </div>
+                <div className='p-col p-row-flex-start p-row-align-top album-details-col'>
+                    <h1 className='album-song-list-title'>
+                        {album.name}
+                    </h1>
+                    <div className = 'album-sub-details'>
+                        {album.artistName}
+                    </div>
+                    <div className = 'album-sub-details'>
+                        2023
+                    </div>
+                    <div className = 'album-sub-details'>
+                        1hr 69m
+                    </div>
+                </div>
+            </div>
             <button onClick={() => closeSongsForAlbumView()}>Back</button>
-            {album.name}
         </div>
         {
             songs && songs.length > 0 &&
-            <>
-                {songs.map((song: Song, index: number) => {
-                    return <div className = {`song-album-row ${index%2==0? 'song-album-even': 'song-album-odd'}`} key = {`${album}-song-${index}`}>
-                        {`#${index} ${song.name}`}
-                    </div>
-                })}
-            </>
+            <table className='song-list'>
+                <thead>
+                    <tr>
+                        <th className='album-songs-table-header-cell'>#</th>
+                        <th className='album-songs-table-header-cell'>Title</th>
+                        <th className='album-songs-table-header-cell'>Plays</th>
+                        <th className='album-songs-table-header-cell'>Length</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {songs.map((song: Song, index: number) => {
+                        return <tr className={`song-album-row ${index % 2 == 0 ? 'song-album-even' : 'song-album-odd'}`} key={`${album}-song-${index}`}>
+                            <td>
+                                <div className='p-row'>
+                                    {index + 1}
+                                </div>
+                            </td>
+                            <td>
+                                {song.name}
+                            </td>
+                            <td>
+                                0
+                            </td>
+                            <td>
+                                0:00
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
 
         }
     </ViewContainer>;
