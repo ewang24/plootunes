e3e73f3789a744012f263ddc6afeccb93972c0ce
E. R. Wang <01ewang24@gmail.com>
2024-04-30 23:35:51 -0600

    read files

---
+++ src/electron/db/fileUtil.ts
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const path = require('path');
+const mm = require('music-metadata');
+import { Database } from "sqlite3";
+import { SemicolonPreference } from "typescript";
+
+export class FileUtil {
+    static async scanFiles(): Promise<void> {
+        //TODO: replace with the path from the config table
+        const libraryPath = 'P:/Music/music/rotation/Abbath';
+        const semaphore = new Semaphore(10);
+        this.readDirectory(libraryPath, semaphore);
+    }
+
+    private static async readDirectory(directoryPath: string, semaphore: Semaphore): Promise<void> {
+        console.log(`Reading from directory ${directoryPath}`);
+        const files = await fs.promises.readdir(directoryPath);
+
+        files.forEach(async (file) => {
+            const filePath = path.join(directoryPath, file);
+
+            fs.stat(filePath, async (err, stats) => {
+                if (err) {
+                    console.error('Error getting file stats:', err);
+                    return;
+                }
+
+                if (stats.isDirectory()) {
+                    // If it's a directory, recursively read its contents
+                    console.log('Found directory to read:', filePath);
+                    this.readDirectory(filePath, semaphore);
+                    return;
+                }
+                else {
+                    await semaphore.acquire();
+                    try {
+                        await this.proccessAudioFile(filePath);
+                    } finally {
+                        semaphore.release();
+                    }
+                }
+            });
+
+        });
+    }
+
+    private static async proccessAudioFile(filePath: string) {
+        const metadata = await mm.parseFile(filePath, { duration: true });
+        fs.appendFileSync('metadata.log', JSON.stringify(metadata, null, 2));
+    }
+}
+
+class Semaphore {
+    private concurrency: number;
+    private tasks: (() => void)[];
+    private currentCount: number;
+
+    constructor(concurrency: number) {
+        this.concurrency = concurrency;
+        this.tasks = [];
+        this.currentCount = 0;
+    }
+
+    acquire(): Promise<void> {
+        return new Promise<void>((resolve) => {
+            this.tasks.push(resolve);
+            this.tryToRun();
+        });
+    }
+
+    release(): void {
+        this.currentCount--;
+        this.tryToRun();
+    }
+
+    private tryToRun(): void {
+        if (this.currentCount < this.concurrency && this.tasks.length > 0) {
+            const task = this.tasks.shift();
+            if (task) {
+                this.currentCount++;
+                task();
+            }
+        }
+    }
+}
--- src/electron/dbtest.ts
+++ src/electron/dbtest.ts
@@ -1,11 +1,14 @@
 import { Database, OPEN_CREATE, OPEN_READWRITE } from "sqlite3";
 import { TableUtil } from "./db/tableUtil";
+import { FileUtil } from "./db/fileUtil";
 
-const db = new Database('P:/Documents/GitHub/psychic-octo-rotary-phone/plootunes.sqlite', OPEN_CREATE | OPEN_READWRITE, (err: Error | null) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-  });
+// const db = new Database('P:/Documents/GitHub/psychic-octo-rotary-phone/plootunes.sqlite', OPEN_CREATE | OPEN_READWRITE, (err: Error | null) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Connected to the in-memory SQlite database.');
+//   });
 
-  TableUtil.createAllTables(db);
+//   TableUtil.createAllTables(db);
+
+FileUtil.scanFiles()
