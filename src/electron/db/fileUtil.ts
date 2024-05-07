40895fe269f954b81f916a56e39dc336347fd73a
E. R. Wang <01ewang24@gmail.com>
2024-05-06 19:51:21 -0600

    make file reading process entirely synchronous so node doesn't run out of memory

--- src/electron/db/fileUtil.ts
+++ src/electron/db/fileUtil.ts
@@ -1,88 +1,85 @@
 const fs = require('fs');
 const path = require('path');
 const mm = require('music-metadata');
-import { Database } from "sqlite3";
-import { SemicolonPreference } from "typescript";
 
 export class FileUtil {
     static async scanFiles(): Promise<void> {
         //TODO: replace with the path from the config table
-        const libraryPath = 'P:/Music/music/rotation/Abbath';
-        const semaphore = new Semaphore(10);
-        this.readDirectory(libraryPath, semaphore);
+        const libraryPath = 'P:/Music/music/rotation';
+        // const semaphore = new Semaphore(10);
+        const output: string[] = [];
+        await this.processLibrary(libraryPath, output);
+        console.log(output, output.length);
     }
 
-    private static async readDirectory(directoryPath: string, semaphore: Semaphore): Promise<void> {
-        console.log(`Reading from directory ${directoryPath}`);
-        const files = await fs.promises.readdir(directoryPath);
+    private static async processLibrary(directoryPath: string, output: string[]): Promise<void> {
+        // console.log(`Reading from directory ${directoryPath}`);
+        output.push(directoryPath);
+        let files = await fs.promises.readdir(directoryPath);
+        const directories = [];
 
-        files.forEach(async (file) => {
+        for(let file of files){    
             const filePath = path.join(directoryPath, file);
+            const stats = await fs.promises.stat(filePath);
+            if (stats.isDirectory()) {
+                // If it's a directory, recursively read its contents
+                // console.log('Found directory to read:', filePath);
+                directories.push(filePath);
+            }
+            else{
+                await this.proccessAudioFile(filePath, output)
+            }
+        }
+        files = null;
 
-            fs.stat(filePath, async (err, stats) => {
-                if (err) {
-                    console.error('Error getting file stats:', err);
-                    return;
-                }
-
-                if (stats.isDirectory()) {
-                    // If it's a directory, recursively read its contents
-                    console.log('Found directory to read:', filePath);
-                    this.readDirectory(filePath, semaphore);
-                    return;
-                }
-                else {
-                    await semaphore.acquire();
-                    try {
-                        await this.proccessAudioFile(filePath);
-                    } finally {
-                        semaphore.release();
-                    }
-                }
-            });
-
-        });
+        for(let dir of directories){
+            await this.processLibrary(dir, output);
+        }
     }
 
-    private static async proccessAudioFile(filePath: string) {
-        const metadata = await mm.parseFile(filePath, { duration: true });
-        fs.appendFileSync('metadata.log', JSON.stringify(metadata, null, 2));
+    private static async proccessAudioFile(filePath: string, output: string[]): Promise<void> {
+        if(path.extname(filePath) === '.mp3'){
+            let metadata = await mm.parseFile(filePath, { duration: true });
+            metadata = null;
+            output.push(filePath);    
+        }
+        
     }
 }
 
 /*
     This semaphore is needed otherwise we will read too many files for node to handle.
 */
-class Semaphore {
-    private concurrency: number;
-    private tasks: (() => void)[];
-    private currentCount: number;
+// class Semaphore {
+//     private concurrency: number;
+//     private tasks: (() => void)[];
+//     private currentCount: number;
 
-    constructor(concurrency: number) {
-        this.concurrency = concurrency;
-        this.tasks = [];
-        this.currentCount = 0;
-    }
+//     constructor(concurrency: number) {
+//         this.concurrency = concurrency;
+//         this.tasks = [];
+//         this.currentCount = 0;
+//     }
 
-    acquire(): Promise<void> {
-        return new Promise<void>((resolve) => {
-            this.tasks.push(resolve);
-            this.tryToRun();
-        });
-    }
+//     acquire(): Promise<void> {
+//         return new Promise<void>((resolve) => {
+//             this.tasks.push(resolve);
+//             this.tryToRun();
+//         });
+//     }
+ 
+//     release(): void {
+//         this.currentCount--;
+//         this.tryToRun();
+//     }
 
-    release(): void {
-        this.currentCount--;
-        this.tryToRun();
-    }
-
-    private tryToRun(): void {
-        if (this.currentCount < this.concurrency && this.tasks.length > 0) {
-            const task = this.tasks.shift();
-            if (task) {
-                this.currentCount++;
-                task();
-            }
-        }
-    }
-}
+//     private tryToRun(): void {
+//         if (this.currentCount < this.concurrency && this.tasks.length > 0) {
+//             const task = this.tasks.shift();
+//             if (task) {
+//                 this.currentCount++;
+//                 task();
+//             }
+//         }
+//     }
+// }
