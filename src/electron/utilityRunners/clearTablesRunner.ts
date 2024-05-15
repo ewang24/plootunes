1055e62b53d3fe129bd3fc99a452e38b1bbb5d29
E. R. Wang <01ewang24@gmail.com>
2024-05-14 18:58:30 -0600

    update clear tables util

--- src/electron/utilityRunners/clearTablesRunner.ts
+++ src/electron/utilityRunners/clearTablesRunner.ts
@@ -3,18 +3,41 @@ import { TableUtil } from "../db/tableUtil";
 import { PropertiesUtil } from "../util/propertiesUtil";
 const fs = require('fs');
 
-fs.unlink(`${PropertiesUtil.initProps()}`, (err) => {
-    if (err) {
-        console.error('Error deleting file:', err);
-        return;
-    }
-    console.log('db file deleted. Preparing to recreate db and tables...');
-});
+async function run() {
 
-const db = new Database(`${PropertiesUtil.initProps()}`, OPEN_CREATE | OPEN_READWRITE, (err: Error | null) => {
+  const dbPath = `${PropertiesUtil.initProps()}`;
+  let re;
+
+  try {
+    console.log('checking db file');
+    await fs.promises.access(dbPath);
+    console.log('db file deleted.');
+    await fs.promises.unlink(`${PropertiesUtil.initProps()}`);
+    re = 're';
+  }
+  catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('db file not present, skipping delete.');
+      re = '';
+    }
+    else{
+      console.error(error);
+    }
+  }
+
+  console.log(`reparing to ${re}create db and tables...`);
+
+  const db = new Database(`${PropertiesUtil.initProps()}`, OPEN_CREATE | OPEN_READWRITE, (err: Error | null) => {
     if (err) {
       return console.error(err.message);
     }
   });
 
-TableUtil.createAllTables(db);
+  console.log('db created');
+
+  TableUtil.createAllTables(db).then(() => {
+    console.log('Done');
+  });
+}
+
+run();
