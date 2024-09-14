35ab3f76ab7a356dc036c8f98719a11ee30ea29d
E. R. Wang <>
2024-09-13 19:28:24 -0600

    adding more interation tests

--- src/electron/__tests__/services/system/librarySetupServiceIntegrationTest.ts
+++ src/electron/__tests__/services/system/librarySetupServiceIntegrationTest.ts
@@ -3,9 +3,10 @@ import { CreateTestDb } from "../../../../core/db/test/util/createTestDb";
 import path = require("path");
 import { CountFilesInTestLibrary } from "../../../../core/db/test/util/countFilesInTestLibrary";
 import { Database, OPEN_READWRITE } from "sqlite3";
-import { DbUtil } from "../../../../core/db/dbUtil";
 import { Sqlite3Connector } from "../../../db/sqlite3Connector";
 import { Connector } from "../../../../core/db/dto/connector";
+import { Album } from "../../../../core/db/dbEntities/album";
+import { Song } from "../../../../core/db/dbEntities/song";
 
 describe("LibrarySetupService", () => {
   jest.setTimeout(30000);
@@ -19,7 +20,7 @@ describe("LibrarySetupService", () => {
         unlinkSync(dbPath);
         console.log('Deleted pre-existing old test database file.');
       } catch (err) {
-        console.error('Error occurred while trying to delete the file:', err);
+        throw new Error(`Failed to delete pre-existing database file: ${err.message}`);
       }
     } else {
       console.log('No old test database file exists.');
@@ -46,7 +47,51 @@ describe("LibrarySetupService", () => {
       const countOfFiles = await CountFilesInTestLibrary.count(libraryPath);
       const dbRecordCount = (await connector.get<number>(`SELECT COUNT(*) FROM song`))["COUNT(*)"];
       expect(dbRecordCount).toEqual(countOfFiles);
+    });
+
+    test("No duplicated songs", async () => {
+      const query = `
+        SELECT name, COUNT(*) FROM song
+        GROUP BY name, songFilePath
+        HAVING COUNT(*) > 1;
+      `;
+
+      const duplicatedSongsCount = await connector.getAll<number>(query);
+      expect(duplicatedSongsCount.length).toEqual(0);
     })
+
+    test("Albums with the same names but different artists create multiple albums", async () => {
+      //This album has 1 song with a different artist. It should have therefore created two albums, one for each artist.
+      const query = `
+        SELECT * FROM album WHERE name = "Blackbraid II";
+      `
+      const albums = await connector.getAll<Album>(query);
+      expect(albums.length).toEqual(2);
+    });
+
+    test("Albums with the same names but different artists should not duplicate songs", async () => {
+      //Same album as above. Make sure that none of the songs between albums are shared. There used to be a bug where it would duplicate all the songs between albums.
+      const query = `
+        SELECT * FROM album WHERE name = "Blackbraid II";
+      `
+      const albums = await connector.getAll<Album>(query);
+      const albumId1 = albums[0].id;
+      const albumId2 = albums[1].id;
+
+      const songsQuery = `SELECT * FROM song where albumId = $albumId`;
+      
+      const [songs1, songs2] = await Promise.all([
+        connector.getAll<Song>(songsQuery, {albumId: albumId1}),
+        connector.getAll<Song>(songsQuery, {albumId: albumId2})
+      ])
+
+      expect(songs1.length).not.toEqual(songs2.length);
+
+      for(let song of songs1){
+        expect(songs2.some((_song)=> _song.name === song.name)).toBeFalsy();
+      }
+    });
+
   });
 
   afterAll(async () => {
