2759184913146af2122f5fcd34c29e820b3285cb
E. R. Wang <>
2024-09-25 01:50:27 -0600

    used react-virtualizer to greatly increase the speed of the songs list page

--- src/react/view/global/widgets/songsGrid.tsx
+++ src/react/view/global/widgets/songsGrid.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 import { Song, SongWithAlbum } from "../../../../core/db/dbEntities/song";
 import PButton from "./pButton";
 import { Icons } from "../../../../core/assets/icons";
+import { AutoSizer, Column, Table } from "react-virtualized";
+import '../../../styles/widgets/songsGrid.scss'
 
 export interface SongsGridProps {
     songs?: SongWithAlbum[];
@@ -12,6 +14,44 @@ export interface SongsGridProps {
 
 function SongsGrid(props: SongsGridProps) {
     const { songs, onPlay, onQueue, displayAlbumInfo } = props;
+
+    function getAlbumArt(albumCoverImageUrl: string | undefined, index: number) {
+        return <div key={index} className='p-tile p-tile-ultra-small'>
+            <div className='p-tile-image'>
+                <>
+                    {albumCoverImageUrl &&
+                        <img draggable="false"
+                            src={`http://localhost:3030/${albumCoverImageUrl}`}
+                        />
+                    }
+                    {!albumCoverImageUrl &&
+                        <>
+                            {index % 2 === 0 &&
+                                <img draggable="false"
+                                    src='../../assets/img/test.jpg'
+                                />
+                            }
+                            {index % 2 !== 0 &&
+                                <img draggable="false"
+                                    src='../../assets/img/up.jpg'
+                                />
+                            }
+                        </>
+                    }
+                </>
+            </div>
+        </div>
+    }
+
+    function renderRowActions(song: Song) {
+        return <div className='p-row'>
+            <PButton label='Play' onClick={() => { onPlay(song) }} icon={Icons.PLAY} displayLabel={false} />
+            <PButton label='Queue' onClick={() => { onQueue(song) }} icon={Icons.PLUS} displayLabel={false} />
+        </div>
+    }
+
+    const totalColumns = 3 + (displayAlbumInfo ? 2 : 0);
+
     return <>
         {(!songs || !songs.length) &&
             <strong>
@@ -20,90 +60,89 @@ function SongsGrid(props: SongsGridProps) {
         }
         {
             songs && songs.length > 0 &&
-            <table className='song-list'>
-                <thead>
-                    <tr>
-                        <th className='album-songs-table-header-cell'></th>
-                        <th className='album-songs-table-header-cell'>#</th>
-                        {displayAlbumInfo &&
-                            <th className='album-songs-table-header-cell'>Album</th>
-                        }
-                        <th className='album-songs-table-header-cell'>Title</th>
-                        {displayAlbumInfo &&
-                            <th className='album-songs-table-header-cell'>Artist</th>
-                        }
-                        <th className='album-songs-table-header-cell'>Plays</th>
-                        <th className='album-songs-table-header-cell'>Length</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {songs.map((song: SongWithAlbum, index: number) => {
-                        return <tr className={`song-album-row ${index % 2 == 0 ? 'song-album-even' : 'song-album-odd'}`} key={song.id}>
-                            <td>
-                                <div className='p-row'>
-                                    <PButton label='Play' onClick={() => { onPlay(song) }} icon={Icons.PLAY} displayLabel={false} />
-                                    <PButton label='Queue' onClick={() => { onQueue(song) }} icon={Icons.PLUS} displayLabel={false} />
-                                </div>
-                            </td>
-                            <td>
-                                <div className='p-row'>
-                                    {index + 1}
-                                </div>
-                            </td>
+            <div className = 'song-grid-virtualizer-container'>
+                <AutoSizer>
+                    {({ height, width }) => {
+                        return <Table height={height}
+                            width={width}
+                            rowCount={songs.length}
+                            rowGetter={({ index }) => songs[index]}
+                            rowHeight={50}
+                            headerHeight={50}
+                            rowClassName={'virtualized-song-grid-row'}
+                        >
+                            <Column
+                                width={width / totalColumns}
+                                disableSort
+                                label="Actions"
+                                dataKey="id"
+                                cellRenderer={({ rowData }) => renderRowActions(rowData)}
+                            // flexGrow={1}
+                            />
+                            <Column
+                                width={width / totalColumns}
+                                disableSort
+                                label="Position"
+                                dataKey="songPosition"
+                                cellRenderer={({ cellData }) => cellData}
+                            // flexGrow={1}
+                            />
                             {displayAlbumInfo &&
-                                <td>
-                                    <div key={index} className='p-tile p-tile-ultra-small'>
-                                        <div className='p-tile-image'>
-                                            <>
-                                                {song.albumCoverImage &&
-                                                    <img draggable="false"
-                                                        src={`http://localhost:3030/${song.albumCoverImage}`}
-                                                    />
-                                                }
-                                                {!song.albumCoverImage &&
-                                                    <>
-                                                        {index % 2 === 0 &&
-                                                            <img draggable="false"
-                                                                src='../../assets/img/test.jpg'
-                                                            />
-                                                        }
-                                                        {index % 2 !== 0 &&
-                                                            <img draggable="false"
-                                                                src='../../assets/img/up.jpg'
-                                                            />
-                                                        }
-                                                    </>
-                                                }
-                                            </>
-                                        </div>
-                                    </div>
-                                </td>
+                                <Column
+                                    width={width / totalColumns}
+                                    disableSort
+                                    label="Album"
+                                    dataKey="albumCoverImage"
+                                    cellRenderer={({ cellData, index }) => { return getAlbumArt(cellData, index) }}
+                                // flexGrow={1}
+                                />
                             }
-                            <td>
-                                {song.name}
-                            </td>
+                            <Column
+                                width={width / totalColumns}
+                                disableSort
+                                label="Title"
+                                dataKey="name"
+                                cellRenderer={({ cellData }) => cellData}
+                            // flexGrow={1}
+                            />
                             {displayAlbumInfo &&
-                            <td>
-                                {song.artistName}
-                            </td>
+                                <Column
+                                    width={width / totalColumns}
+                                    disableSort
+                                    label="Artist "
+                                    dataKey="artistName"
+                                    cellRenderer={({ cellData }) => cellData}
+                                // flexGrow={1}
+                                />
                             }
-                            <td>
-                                0
-                            </td>
-                            <td>
-                                {getLength(song)}
-                            </td>
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-
+                            {/* <Column
+                                disableSort
+                                label="Plays"
+                                dataKey="plays"
+                                cellRenderer={({ plays }) => plays}
+                                flexGrow={1}
+                            /> */}
+                            <Column
+                                width={width / totalColumns}
+                                disableSort
+                                label="Length"
+                                dataKey="songLength"
+                                cellRenderer={({ length }) => getLength(length)}
+                            // flexGrow={1}
+                            />
+                        </Table>
+                    }}
+                </AutoSizer>
+            </div>
         }
     </>
 }
 
-function getLength(song: Song): string {
-    return `${Math.floor(song.songLength / 60)}:${Math.floor(song.songLength % 60).toString().padStart(2, '0')}`
+function getLength(songLength: number | undefined): string {
+    if (songLength === undefined) {
+        return '0:00';
+    }
+    return `${Math.floor(songLength / 60)}:${Math.floor(songLength % 60).toString().padStart(2, '0')}`
 }
 
 export default SongsGrid;
